b3e1cc5b9cbc61193026ad6b5b12e27baed71cbc
Yoe <dembouz719@gmail.com>
2024-05-31 20:11:48 +0200

    added new components AddMovieForm

--- myMovieDB/src/App.tsx
+++ myMovieDB/src/App.tsx
@@ -3,21 +3,34 @@ import axios from 'axios';
 import './styles/index.css';
 
 function App() {
-  //const [count, setCount] = useState(0)
-
-  const key = 'movieDB-hj64bh';
+  const [key, setKey] = useState('');
+  const [movies, setMovies] = useState([]);
 
   useEffect (() => {
+    axios.get(`http://localhost:8080/api/keys`)
+    .then(response => {
+      console.log('API Key Response:', response.data);
+      setKey(response.data.data);
+    });
+  }, []);
+
+  useEffect (() => {
+    if(key) {
+      console.log('API Key:', key);
     axios.get(`http://localhost:8080/api/movies?key=${key}`)
     .then(response => {
-      console.log(response.data);
-    })
+      console.log('Movies Response:', response.data);
+      setMovies(response.data);
+    });
+  }
 
-  });
+  }, [key]);
 
   return (
-   <div>Test</div>
+    <div> 
+      <h1>Movie App</h1>
+    </div>
   )
 }
 
-export default App
+export default App;
